797457ab49a6e1b4d418c5e7e5ea930feab9adb2
rahadkc <rahadkc@gmail.com>
2025-02-22 03:49:52 +0600

    Constant: Endpoints and Routes constant add

---
+++ src/app/lib/constants.ts
@@ -0,0 +1,14 @@
+// Routes
+
+export const ROUTES = {
+  dashboard: '/dashboard',
+  onboard: '/onboard',
+} as const;
+
+export const ENDPOINTS = {
+  auth: '/login',
+  summary: '/dashboard/summary',
+  stat: '/dashboard/stat',
+  offer: '/offers',
+  users: '/users',
+} as const;
