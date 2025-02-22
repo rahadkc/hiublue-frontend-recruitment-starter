00e72e33f3216779b52dd098c601aadd45c8fc48
rahadkc <rahadkc@gmail.com>
2025-02-23 02:01:26 +0600

    Theme: color palette and custom-shadow update

--- src/theme/palette.ts
+++ src/theme/palette.ts
@@ -1,5 +1,5 @@
 import { alpha } from '@mui/material/styles';
-import { PaletteOptions } from '@mui/material/styles/createPalette';
+import type { PaletteOptions } from '@mui/material/styles/createPalette';
 
 // ----------------------------------------------------------------------
 
@@ -21,8 +21,6 @@ declare module '@mui/material/styles/createPalette' {
   }
 }
 
-// SETUP COLORS
-
 export const grey = {
   0: '#FFFFFF',
   100: '#F9FAFB',
@@ -32,25 +30,25 @@ export const grey = {
   500: '#919EAB',
   600: '#637381',
   700: '#454F5B',
-  800: '#212B36',
+  800: '#1C252E',
   900: '#161C24',
 };
 
 export const primary = {
-  lighter: '#D1E9FC',
-  light: '#76B0F1',
-  main: '#2065D1',
-  dark: '#103996',
-  darker: '#061B64',
+  lighter: '#B9EBD9',
+  light: '#66D1AC',
+  main: '#00A76F',
+  dark: '#007A57',
+  darker: '#004E38',
   contrastText: '#FFFFFF',
 };
 
 export const secondary = {
-  lighter: '#EFD6FF',
-  light: '#C684FF',
-  main: '#8E33FF',
-  dark: '#5119B7',
-  darker: '#27097A',
+  lighter: '#4A4F56',
+  light: '#333A42',
+  main: '#1C252E',
+  dark: '#161C24',
+  darker: '#0D1117',
   contrastText: '#FFFFFF',
 };
 
@@ -78,7 +76,7 @@ export const warning = {
   main: '#FFAB00',
   dark: '#B76E00',
   darker: '#7A4100',
-  contrastText: '#ffffff',
+  contrastText: '#FFFFFF',
 };
 
 export const error = {
@@ -91,7 +89,7 @@ export const error = {
 };
 
 export const common = {
-  black: '#000000',
+  black: '#1C252E',
   white: '#FFFFFF',
 };
 
@@ -116,7 +114,7 @@ export const palette: PaletteOptions = {
   error,
   grey,
   common,
-  divider: alpha(grey[500], 0.2),
+  divider: alpha(grey[500], 0.12),
   mode: 'light',
   text: {
     primary: grey[800],
@@ -125,7 +123,7 @@ export const palette: PaletteOptions = {
   },
   background: {
     paper: '#FFFFFF',
-    default: '#FFFFFF',
+    default: '#F4F6F8',
     neutral: grey[200],
   },
   action: {
