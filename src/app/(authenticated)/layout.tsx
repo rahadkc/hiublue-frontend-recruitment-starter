8f70154e132447bec2dfde9b81097265d1dd1693
rahadkc <rahadkc@gmail.com>
2025-02-28 00:06:28 +0600

    Dashboard: main dashboard view updated and layout structure modified

--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -1,16 +1,20 @@
+'use client';
 import Header from '@/components/layout/header';
 import Sidebar from '@/components/layout/sidebar';
+import { SidebarProvider } from '@/contexts/sidebar-context';
 import { Box } from '@mui/material';
 import { ReactNode } from 'react';
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
-    <Box sx={{ display: 'flex' }}>
-      <Sidebar />
-      <Box sx={{ flexGrow: 1 }}>
-        <Header />
-        <Box sx={{ p: 3 }}>{children}</Box>
+    <SidebarProvider>
+      <Box sx={{ display: 'flex' }}>
+        <Sidebar />
+        <Box sx={{ flexGrow: 1 }}>
+          <Header />
+          <Box sx={{ p: 3 }}>{children}</Box>
+        </Box>
       </Box>
-    </Box>
+    </SidebarProvider>
   );
 }
--- src/sections/dashboard/views/dashboard-view.tsx
+++ src/sections/dashboard/views/dashboard-view.tsx
@@ -4,26 +4,43 @@ import WebsiteVisitsChart from '@/sections/dashboard/website-visits';
 import { CardHeader } from '@mui/material';
 import Card from '@mui/material/Card';
 import Grid from '@mui/material/Grid2';
+import OfferList from '../offer-list';
 import OffersSentChart from '../offers-sent';
+import ColumnGroupingTable from '../table-test';
 
 export default function DashboardView() {
   return (
     <Grid sx={{ display: 'flex', flexDirection: 'column', gap: 3 }}>
       <Summary />
       <Grid container spacing={2}>
-        <Grid size={6}>
+        <Grid size={{ xs: 12, md: 6 }}>
           <Card>
             <CardHeader title={WIDGET_TEXT.VISIT_STAT} sx={{ paddingBottom: 3 }} />
             <WebsiteVisitsChart />
           </Card>
         </Grid>
-        <Grid size={6}>
+        <Grid size={{ xs: 12, md: 6 }}>
           <Card>
             <CardHeader title={WIDGET_TEXT.OFFER_STAT} sx={{ paddingBottom: 3 }} />
             <OffersSentChart />
           </Card>
         </Grid>
+
+        <Grid size={12}>
+          <Card>
+            <CardHeader title={WIDGET_TEXT.OFFER_LIST} sx={{ paddingBottom: 3 }} />
+            <OfferList />
+          </Card>
+        </Grid>
       </Grid>
+      {/* <Grid container>
+        <Grid size={4} sx={{ flexWrap: 'wrap' }}>
+          <Card>
+            <CardHeader title={WIDGET_TEXT.OFFER_LIST} sx={{ paddingBottom: 3 }} />
+            <OfferList />
+          </Card>
+        </Grid>
+      </Grid> */}
     </Grid>
   );
 }
